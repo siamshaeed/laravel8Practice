a0770d61068ed805e018a164ed163de4e73bdd95
siamshaeed <siamshaeed@gmail.com>
2022-02-10 00:41:50 +0600

    git test

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// git test
 Route::get('/', function () {
     return view('welcome');
 });
