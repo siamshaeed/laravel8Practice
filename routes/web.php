d5fe2c2186bdf7c6b701537b2e111048cc230257
siamshaeed <siamshaeed@gmail.com>
2022-02-15 22:54:04 +0600

    test

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ use App\Http\Controllers\RelationshipController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+
+siam
 */
 Route::get('/', function () {
     return view('welcome');
